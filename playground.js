59508c0a701c3e9c7dba5d13b492d4e201b14553
shermmanm0 <shermanm0@gmail.com>
2020-01-02 20:31:11 -0500

    finished sql to power query script

--- playground.js
+++ playground.js
@@ -1,10 +1,17 @@
-var string = "inner join cycle_day cyc on cyc.id = cal.cycle_day_id\n"
-string = string.trim();
-var ssStart = string.indexOf("join")+5;
-var ssEnd = string.indexOf("on");
-var substr = string.substring(ssStart);
-var substrArr = substr.split(' on ',)
-var substrArr2 = substrArr[1].split(' = ')
-substrArr[1]=substrArr2[0];
-substrArr.push(substrArr2[1]);
-console.log(substrArr);
+var arrayOfObjects = [
+    {
+        "name": "Rose",
+        "age": "5 months"
+    },
+    {
+        "name": "Wanda",
+        "age": "398 months"
+    },
+    {
+        "name": "Matt",
+        "age": "389 months"
+    }
+]
+
+
+console.log(arrayOfObjects.find(element => element.name === "Wanda").age);
--- sqlToPowerQuery.js
+++ sqlToPowerQuery.js
@@ -1,97 +1,55 @@
 //import pq from 'powerQuery.js';
 const fs = require ('fs');
-const lineReader = require ('line-reader')
-var tableNames = [];
-var fieldNames = [];
+const _ = require('lodash');
 
-
-
-function endOfQuery(line){
-    return line.startsWith('where') || line.startsWith('order') || (line == null)
-}
-
-function lineIsFrom(line){
-    return line.toLowerCase().startsWith('from')
+function getQueriesFromSQL(fileName){
+    const rawData = fs.readFileSync(fileName,'utf8').toLowerCase();
+    let fields = [];
+    let tables = [];
+    let args = [];
+    const fieldsRegEx = new RegExp(/[a-z_]+\.[a-z_]+/g)
+    const tablesRegEx = new RegExp(/((from)\s[a-z_]+\s+[a-z_]+)|((inner join)\s[a-z_]+\s+[a-z_]+)/g);
+    const argsRegEx = new RegExp(/:[a-z_]+/g);
+    fields = rawData.match(fieldsRegEx);
+    tables = rawData.match(tablesRegEx);
+    args = rawData.match(argsRegEx);
+    if(args!=null){
+        args = args.map(x =>{return x.substring(1,x.length)});
+    }
+    fields = _.uniq(fields);
+    tables = _.uniq(tables);
+    args = _.uniq(args);
+    let tablesProcessed = tables.map(tableString =>{
+        if (tableString.startsWith("from ")){
+            tableString = tableString.substring(5,tableString.length);    
+        }
+        else if (tableString.startsWith("inner join ")){
+            tableString = tableString.substring(11,tableString.length);
+        }
+        let tableStringArray = tableString.split(" ")
+    
+        let tableObject = {
+            "name": tableStringArray[0],
+            "abbreviation": tableStringArray[1] 
+        }
+        return tableObject
+    })
+    let fieldsProcessed = fields.map(fieldString =>{
+       fieldStringArray = fieldString.split('.');
+       fieldObject = {
+            "table": tablesProcessed.find(table => table.abbreviation === fieldStringArray[0]).name,
+            "name": fieldStringArray[1]
+        }
+        return fieldObject
+    })
+    
+    
+    return {
+        "fields": _.sortBy(fieldsProcessed,['table','name']),
+        "args": args.sort()
+    }
+    
     
 }
-function lineIsAJoin(line){
-    return line.toLowerCase().startsWith('join') ||
-    line.toLowerCase().startsWith('inner join') ||
-    line.toLowerCase().startsWith('left join') ||
-    line.toLowerCase().startsWith('right join')
-}
 
-
-
-lineReader.eachLine('testsql.sql', function(line, last) {
-line = line.toLowerCase();
-    //console.log(line);
-    if (endOfQuery(line)) {
-        fieldNames.forEach(field =>{
-            tableNames.forEach(table =>{
-                if (field.tableAbbreviation == table.tableAbbreviation){
-                    field.tableName = table.tableName;
-                }
-            })
-        })
-        console.log(fieldNames);
-      return false; // stop reading
-    }
-    else if (lineIsFrom(line)){
-        let temp = line.replace('from','')
-        temp = temp.trim();
-        let tempObject = {
-            'tableName': temp.split(' ')[0],
-            'tableAbbreviation': temp.split(' ')[1]
-        }
-        tableNames.push(tempObject);
-    }
-    else if (lineIsAJoin(line)){
-        let string = line.trim();
-        let ssStart = string.indexOf("join")+5;
-        let substr = string.substring(ssStart);
-        let substrArr = substr.split(' on ',)
-        let substrArr2 = substrArr[1].split(' = ')
-        let tempObject1 = {
-            'tableName': substrArr[0].split(' ')[0],
-            'tableAbbreviation': substrArr[0].split(' ')[1]
-        }
-        let tempObject2 = {            
-            'tableAbbreviation': substrArr2[0].split('.')[0],
-            'fieldName': substrArr2[0].split('.')[1],
-            'tableName': ""
-        }
-        let tempObject3 = {            
-            'tableAbbreviation': substrArr2[1].split('.')[0],
-            'fieldName': substrArr2[1].split('.')[1],
-            'tableName': ""
-        }
-        tableNames.push(tempObject1);
-        fieldNames.push(tempObject2);
-        fieldNames.push(tempObject3);
-
-    }
-    else if (line.endsWith(',\n')){
-        let temp = line.trim()
-        temp = temp.substr(0,temp.length-1);
-        let tempObject ={
-            'tableAbbreviation': temp.split('.')[0],
-            'fieldName': temp.split('.')[1],
-            'tableName': ""
-        }
-        fieldNames.push(tempObject);
-        
-    }
-    else{
-        let temp = line.trim()
-        temp = temp.substr(0,temp.length);
-        let tempObject ={
-            'tableAbbreviation': temp.split('.')[0],
-            'fieldName': temp.split('.')[1],
-            'tableName': ""
-        }
-        fieldNames.push(tempObject);
-               
-    }
-
-  });
+console.log(getQueriesFromSQL('testsql2.txt'));
